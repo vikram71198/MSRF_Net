c7e16fb96c164505ebe94e99fdda3bd52e56cc85
Madhav Kanbur <abcdjdj@gmail.com>
2022-04-30 13:32:08 -0500

    Use manual seed, eval() mode
    Signed-off-by: Madhav Kanbur <abcdjdj@gmail.com>

--- euler_inference.py
+++ euler_inference.py
@@ -16,15 +16,17 @@ import os
 from matplotlib import pyplot as plt
 import shutil
 
+torch.manual_seed(0)
+#torch.use_deterministic_algorithms(True)
+
 # Use GPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Change directory to /srv/home/kanbur/MSRF_Net
 os.chdir('/srv/home/kanbur/MSRF_Net')
 
-msrf_net = MSRF(in_ch = 3, n_classes = 2, init_feat = 32)
+msrf_net = MSRF(in_ch = 3, n_classes = 2, init_feat = 32).eval().to(device)
 msrf_net.load_state_dict(torch.load("trained_models/msrf_cvc-clinic.pt"))#, map_location=torch.device('cpu')))
-msrf_net = msrf_net.to(device)
 
 
 def load_split_sets():
@@ -41,15 +43,16 @@ def load_split_sets():
 def infer(input_filenames):
     print('Files = ' + str(input_filenames))
     x = [euler_utils.img_to_tensor(euler_utils.read_img(ele[0])) for ele in input_filenames]
-    x = torch.cat(x, dim = 0).to(device)
+    x = torch.cat(x, dim = 0)
     x = x.float()
+    x = x.to(device)
     #print(f'X Shape = {x.shape}')
 
     canny_x = [torch.FloatTensor(np.asarray(cv2.Canny(cv2.imread(ele[0]), 10, 100), np.float32)/255.0) for ele in input_filenames]
-    #cv2.imwrite('canny.png', canny_x[0].numpy())
+    cv2.imwrite('canny.png', canny_x[0].numpy() * 255)
     canny_x = [t.view(-1, t.shape[0], t.shape[1]) for t in canny_x]
     canny_x = torch.stack(canny_x, dim = 0).to(device)
-    canny_x = canny_x * 0
+    #canny_x = canny_x * 0
 
     with torch.no_grad():
         y_1, y_2, y_3, y_canny = msrf_net(x, canny_x)
@@ -65,4 +68,4 @@ def infer(input_filenames):
 
 
 train_set, val_set, test_set = load_split_sets()
-infer([test_set[2]])
+infer([test_set[0]])#, test_set[1], test_set[2], test_set[3], test_set[4], test_set[5], test_set[6], test_set[7]])
